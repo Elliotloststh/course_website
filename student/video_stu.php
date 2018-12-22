711e9db59668767e343e2166ed7b008ec10abc13
Rothyu <1144358492@qq.com>
2018-12-22 11:37:38 +0800

    Add files via upload

--- student/video_stu.php
+++ student/video_stu.php
@@ -2,12 +2,10 @@
 /**
  * Created by PhpStorm.
  * User: 黄亦非
- * Date: 2018/12/7
- * Time: 23:27
+ * Date: 2018/12/8
+ * Time: 13:36
  */
-require_once('../common/mysql_connect.php');
 session_start();
-
 ?>
 
 <!DOCTYPE html>
@@ -298,7 +296,7 @@ session_start();
                         <a href="JiaoXueDaGang_stu.php"><i class="fa fa-eject fa-fw"></i> 教学大纲</a>
                     </li>
                     <li>
-                        <a href="forms.html"><i class="fa fa-edit fa-fw"></i> 课程资料<span class="fa arrow"></span></a>
+                        <a href="#"><i class="fa fa-edit fa-fw"></i> 课程资料<span class="fa arrow"></span></a>
                         <ul class="nav nav-second-level">
                             <li>
                                 <a href="KeJian_stu.php"> 课件</a>
@@ -354,79 +352,65 @@ session_start();
         <!-- /.navbar-static-side -->
     </nav>
 
-<!--    右侧工作区  -->
+    <!--    右侧工作区  -->
     <div id="page-wrapper">
         <div class="row">
             <div class="col-lg-12">
-                <div class="panel panel-default">
+                <div class="panel panel-info">
                     <div class="panel-heading">
-                       <h4>课程简介</h4>
+                        <strong>教学视频</strong>
                     </div>
-                    <!-- /.panel-heading -->
                     <div class="panel-body">
                         <!-- Nav tabs -->
                         <ul class="nav nav-tabs">
-                            <li class="active"><a href="#home" data-toggle="tab">参考教材</a>
-                            </li>
-                            <li><a href="#profile" data-toggle="tab">成绩评定</a>
-                            </li>
-                            <li><a href="#messages" data-toggle="tab">实验</a>
-                            </li>
-                            <li><a href="#brief" data-toggle="tab">简介信息</a>
+                            <li class="active"><a href="#home" data-toggle="tab">观看视频</a>
                             </li>
                         </ul>
 
                         <!-- Tab panes -->
                         <div class="tab-content">
                             <div class="tab-pane fade in active" id="home">
-                                <h5>
-                                    《软件需求（第2版）》[美] Karl E. Wiegers 著，刘伟琴、刘洪涛译，清华大学出
-                                    版社，2004 年 11 月第 1 版<br/><br/>
-                                    《计算机软件文档编制规范》<br/><br/>
-                                    《软件需求规格说明书》<br/><br/>
-                                    《软件工程管理》
-                                </h5>
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered table-hover">
+                                        <thead>
+                                        <tr>
+                                            <th>标题</th>
+                                            <th>类型</th>
+                                            <th>操作</th>
+                                        </tr>
+                                        </thead>
+                                        <tbody>
+                                        <?php
+                                        require_once('../common/mysql_connect.php');
+                                        $query = 'select * from material WHERE class_id = '.$_SESSION['class_id'];
+                                        $result = mysqli_query($conn, $query);
+                                        while ($row = mysqli_fetch_assoc($result))
+                                        {
+                                            echo '<tr>';
+                                            echo "<td>".$row['material_name']."</td>";
+                                            echo "<td>".$row['material_type']."</td>";
+                                            echo "<td><a target='_blank' href='../common/watch_video.php?addr=".$row['local_address']."'>观看视频</a> </td>";
+                                            echo '</tr>';
+                                        }
+                                        mysqli_close($conn);
+                                        ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                                <!-- /.table-responsive -->
                             </div>
                             <div class="tab-pane fade" id="profile">
-                                <h5>
-                                    期末考试：50%<br><br>
-                                    作业（homework）：10%<br><br>
-                                    课堂练习：7%<br><br>
-                                    实验报告：9%<br><br>
-                                    实验验收：12%<br><br>
-                                    课堂演讲：12%<br><br>
-                                </h5>
-                            </div>
-                            <div class="tab-pane fade" id="messages">
-                                <h5>
-                                    上机、验收时间周六：下午7、8节<br><br>
-                                    地点： 玉泉校区计算机学院机房，曹西503机房， 本人计算机<br><br>
-                                    实验验收在周六下午，地点曹西503机房<br><br>
-                                    助教在周六总是在机房<br><br>
-                                </h5>
-                            </div>
-                            <div class="tab-pane fade" id="brief">
-                                <h5 style="margin: 10px;line-height: 30px">
-                                    <?php
-                                    require_once('../common/mysql_connect.php');
-                                    $course = $_SESSION['course_id'];
-                                    $query = 'select introduction from course WHERE course_id = '.$course;
-                                    $result = mysqli_query($conn, $query);
-                                    $intro = (mysqli_fetch_assoc($result))['introduction'];
-                                    echo $intro;
-                                    ?>
-                                </h5>
+
                             </div>
                         </div>
                     </div>
-                    <!-- /.panel-body -->
                 </div>
-                <!-- /.panel -->
             </div>
+            <!-- /.col-lg-4 -->
         </div>
+        <!-- /.row -->
     </div>
     <!-- /#page-wrapper -->
-
 </div>
 <!-- /#wrapper -->
 
@@ -450,4 +434,3 @@ session_start();
 </body>
 
 </html>
-
