1c69352b37d81f45dacd3e9ba3fc364f271ad044
veronicatjan <veronicatjan@hotmail.com>
2018-12-25 04:36:43 +0800

    Designed Delete group member/group's page UI, fixed some bugs

--- teacher/group_manage copy.php
+++ teacher/group_manage copy.php
@@ -1,8 +1,5 @@
 <?php
 require_once('../common/mysql_connect.php');
-$query = 'select name, stu_number, telephone, email, introduction from people WHERE people_id = 1';
-$result = mysqli_query($conn, $query);
-
 ?>
 
 <!DOCTYPE html>
@@ -273,7 +270,7 @@ $result = mysqli_query($conn, $query);
             <div class="sidebar-nav navbar-collapse">
                 <ul class="nav" id="side-menu">
                     <li>
-                        <a href="class_notice_tea.php"><i class="fa fa-dashboard fa-fw"></i> 课程公告</a>
+                        <a href="gonggao_stu.php"><i class="fa fa-dashboard fa-fw"></i> 课程公告</a>
                     </li>
                     <li>
                         <a href="index.html"><i class="fa fa-dashcube fa-fw"></i> 课程信息<span class="fa arrow"></span></a>
@@ -304,7 +301,16 @@ $result = mysqli_query($conn, $query);
                         </ul>
                     </li>
                     <li>
-                        <a href="group_list.php"><i class="fa fa-users fa-fw"></i> 课程小组</a>
+                        <a href="#"><i class="fa fa-wrench fa-fw"></i> 课程小组<span class="fa arrow"></span></a>
+                        <ul class="nav nav-second-level">
+                            <li>
+                                <a href="group_list.php"> 小组列表</a>
+                            </li>
+                            <li>
+                                <a href="group_manage.php"> 添加小组</a>
+                            </li>
+                        </ul>
+                        <!-- /.nav-second-level -->
                     </li>
                     <li>
                         <a href="homework.php"><i class="fa fa-tablet fa-fw"></i> 作业管理</a>
@@ -343,102 +349,52 @@ $result = mysqli_query($conn, $query);
     <!--    右侧工作区  -->
     <div id="page-wrapper">
         <div class="row">
-                <div class="col-lg-12">
-                    <div class="panel panel-primary">
-                        <div class="panel-heading">
-                            <i class="fa fa-users fa-fw"></i>小组信息 
-                            <div class="btn-group pull-right">
-                                <button type="button" class="btn btn-default btn-xs " >
-                                    <a href="group_manage.php">
-                                    编辑删除 <i class="fa fa-times-circle"></i>
-                                    </a>
-                                </button>
-                            </div>
-                        </div>
-                        <!-- .panel-heading -->
-                        <div class="panel-body">
-                            <div class="panel-group" id="accordion">
-                                 <!-- .panel-info -->
-                                 <?php 
-                                    require_once('../common/mysql_connect.php');
-                                    $query = 'select distinct group_id FROM student_class_group WHERE class_id = 1';
-                                    $result = mysqli_query($conn, $query);
-                                    $group_id_array = array();
-                                    while($row = mysqli_fetch_assoc($result)) array_push($group_id_array, $row['group_id']);
-                                    $Collapse_Alphabet = array("One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen","Twenty");
-                                    // Collapse_Alphabet is used for the collapsible effect 
-                                    for ($i = 0; $i < count($group_id_array); $i++)
-                                    {
-                                        $group_id = $group_id_array[$i];
-                                    echo '<div class="panel panel-info">';
-                                        echo '<div class="panel-heading">';
-                                            echo '<h4 class="panel-title">';
-                                            if($group_id < 10){
-                                                echo '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$Collapse_Alphabet[$i].'">小组人员信息 G0'.$group_id.'</a>';
-                                            }
-                                            else {
-                                                echo '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$Collapse_Alphabet[$i].'">小组人员信息 G'.$group_id.'</a>';
-                                            }
-                                            echo '</h4>';
-                                        echo '</div>';
-
-                                         echo '<div id="collapse'.$Collapse_Alphabet[$i].'" class="panel-collapse collapse">';
-                                            echo '<div class="panel-body"> ';
-                                                echo '<div class="table-responsive">';
-                                                    echo '<table class="table table-striped table-hover">';
-                                                        echo '<thead>';
-                                                        echo '<tr>';
-                                                            echo '<th>#</th>';
-                                                            echo '<th>姓名</th>';
-                                                            echo '<th>学号</th>';
-                                                        echo '</tr>';
-                                                        echo '</thead>';
-                                                        echo '<tbody>';
-                                        $query = 'select student_id from student_class_group WHERE class_id = 1 AND group_id = '.$group_id;
-                                        $result = mysqli_query($conn, $query);
-                                        $student_id_array = array();
-                                        while ($row = mysqli_fetch_assoc($result)) array_push($student_id_array, $row['student_id']);
-                                        for($j=0; $j < count($student_id_array) ; $j++ )
-                                        {
-                                            $no = $j + 1;
-                                            $student_id = $student_id_array[$j];
-                                            $query = "select stu_number from people WHERE people.people_id = $student_id ";
-                                            $result = mysqli_query($conn, $query);
-                                            $stu_number = mysqli_fetch_assoc($result)['stu_number'];
-                                            $query = 'select name from people WHERE people.people_id = '.$student_id ;
-                                            $result = mysqli_query($conn, $query);
-                                            $name = mysqli_fetch_assoc($result)['name'];
-                                                        echo '<tr>';
-                                                            echo '<td>'.$no.'</td>';
-                                                            echo '<td>'.$name.'</td>';
-                                                            echo '<td>'.$stu_number.'</td>';
-                                                        echo '</tr>';
-                                        }
-                                                        echo '</tbody>';
-                                                    echo '</table>';
-                                                echo '</div>';
-                                                echo ' <a href="#">';
-                                                                echo '<i class="fa fa-plus fa-fw"></i> 添加小组成员 <i class="fa fa-user fa-fw"></i>';
-                                                        echo '</a>';
-                                            echo '</div>';
-                                        echo '</div>';
-                                echo '</div>';
-                                }
-                                ?>
-                                <!-- /.panel-info -->
-                            </div>
-                                <div class="panel-footer">     
-                                    <a href="#">
-                                            <i class="fa fa-plus fa-fw"></i> 添加小组 <i class="fa fa-users fa-fw"></i>
-                                    </a>
-                            </div>
-                        </div>
-                        <!-- .panel-body -->
+            <div class="col-lg-8" style="line-height: 300%">
+                <div class="panel panel-info">
+                    <div class="panel-heading">
+                        <strong>添加学生</strong>
                     </div>
-                    <!-- /.panel -->
+                    <div class="panel-body">
+                        <form method="post" action="">
+                        <div class="form-group">
+                            <label>选择小组</label>
+                            <select class="form-control" name="select_group">
+                                <option>G01</option>
+                                <option>G02</option>
+                                <option>G03</option>
+                            </select>
+                        </div>
+                        <div class="form-group input-group">
+                            <span class="input-group-addon">学生姓名</span>
+                            <input class="form-control" type="text" placeholder="name" name="stu_name">
+                        </div>
+                        <center><button class="btn btn-success" type="submit">确认添加</button></center>
+                        </form>
+                    </div>
                 </div>
-                <!-- /.col-lg-12 -->
             </div>
+            <div class="col-lg-8" style="line-height: 300%">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <strong>添加小组</strong>
+                    </div>
+                    <div class="panel-body">
+                        <form method="post" action="">
+                            <div class="form-group input-group">
+                                <span class="input-group-addon">小组名</span>
+                                <input class="form-control" type="text" placeholder="name" name="group_name">
+                            </div>
+                            <div class="form-group input-group">
+                                <span class="input-group-addon">小组序号</span>
+                                <input class="form-control" type="text" placeholder="id" name="group_id">
+                            </div>
+                            <center><button class="btn btn-sm" type="submit">确认添加</button></center>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <!-- /.col-lg-4 -->
+        </div>
         <!-- /.row -->
     </div>
     <!-- /#page-wrapper -->
@@ -465,3 +421,6 @@ $result = mysqli_query($conn, $query);
 </body>
 
 </html>
+
+
+
--- teacher/group_manage.php
+++ teacher/group_manage.php
@@ -1,5 +1,8 @@
 <?php
 require_once('../common/mysql_connect.php');
+$query = 'select name, stu_number, telephone, email, introduction from people WHERE people_id = 1';
+$result = mysqli_query($conn, $query);
+
 ?>
 
 <!DOCTYPE html>
@@ -270,7 +273,7 @@ require_once('../common/mysql_connect.php');
             <div class="sidebar-nav navbar-collapse">
                 <ul class="nav" id="side-menu">
                     <li>
-                        <a href="gonggao_stu.php"><i class="fa fa-dashboard fa-fw"></i> 课程公告</a>
+                        <a href="class_notice_tea.php"><i class="fa fa-dashboard fa-fw"></i> 课程公告</a>
                     </li>
                     <li>
                         <a href="index.html"><i class="fa fa-dashcube fa-fw"></i> 课程信息<span class="fa arrow"></span></a>
@@ -301,16 +304,7 @@ require_once('../common/mysql_connect.php');
                         </ul>
                     </li>
                     <li>
-                        <a href="#"><i class="fa fa-wrench fa-fw"></i> 课程小组<span class="fa arrow"></span></a>
-                        <ul class="nav nav-second-level">
-                            <li>
-                                <a href="group_list.php"> 小组列表</a>
-                            </li>
-                            <li>
-                                <a href="group_manage.php"> 添加小组</a>
-                            </li>
-                        </ul>
-                        <!-- /.nav-second-level -->
+                        <a href="group_list.php"><i class="fa fa-users fa-fw"></i> 课程小组</a>
                     </li>
                     <li>
                         <a href="homework.php"><i class="fa fa-tablet fa-fw"></i> 作业管理</a>
@@ -349,52 +343,103 @@ require_once('../common/mysql_connect.php');
     <!--    右侧工作区  -->
     <div id="page-wrapper">
         <div class="row">
-            <div class="col-lg-8" style="line-height: 300%">
-                <div class="panel panel-info">
-                    <div class="panel-heading">
-                        <strong>添加学生</strong>
-                    </div>
-                    <div class="panel-body">
-                        <form method="post" action="">
-                        <div class="form-group">
-                            <label>选择小组</label>
-                            <select class="form-control" name="select_group">
-                                <option>G01</option>
-                                <option>G02</option>
-                                <option>G03</option>
-                            </select>
+                <div class="col-lg-12">
+                    <div class="panel panel-primary">
+                        <div class="panel-heading">
+                            <i class="fa fa-users fa-fw"></i>小组信息 
                         </div>
-                        <div class="form-group input-group">
-                            <span class="input-group-addon">学生姓名</span>
-                            <input class="form-control" type="text" placeholder="name" name="stu_name">
+                        <!-- .panel-heading -->
+                        <div class="panel-body">
+                            <div class="panel-group" id="accordion">
+                                 <!-- .panel-info -->
+                                 <?php 
+                                    require_once('../common/mysql_connect.php');
+                                    $query = 'select distinct group_id FROM student_class_group WHERE class_id = 1';
+                                    $result = mysqli_query($conn, $query);
+                                    $group_id_array = array();
+                                    while($row = mysqli_fetch_assoc($result)) array_push($group_id_array, $row['group_id']);
+                                    $Collapse_Alphabet = array("One","Two","Three","Four","Five","Six","Seven","Eight","Nine","Ten","Eleven","Twelve","Thirteen","Fourteen","Fifteen","Sixteen","Seventeen","Eighteen","Nineteen","Twenty");
+                                    // Collapse_Alphabet is used for the collapsible effect 
+                                    for ($i = 0; $i < count($group_id_array); $i++)
+                                    {
+                                        $group_id = $group_id_array[$i];
+                                    echo '<div class="panel panel-info">';
+                                        echo '<div class="panel-heading">';
+                                            echo '<h4 class="panel-title">';
+                                            if($group_id < 10){
+                                                echo '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$Collapse_Alphabet[$i].'">小组人员信息 G0'.$group_id.'</a>';
+                                                    echo'<div class="btn-group pull-right">';
+                                                        echo'<button type="button" class="btn btn-default btn-xs">';
+                                                            echo'<a href="group_.php">';
+                                                            echo'删除 <i class="fa fa-times-circle"></i>';
+                                                            echo '</a>';
+                                                       echo'</button>';
+                                                    echo'</div>';
+                                            }
+                                            else {
+                                                echo '<a data-toggle="collapse" data-parent="#accordion" href="#collapse'.$Collapse_Alphabet[$i].'">小组人员信息 G'.$group_id.'</a>';
+                                                    echo'<div class="btn-group pull-right">';
+                                                        echo'<button type="button" class="btn btn-default btn-xs">';
+                                                            echo'<a href="group_.php">';
+                                                            echo'删除 <i class="fa fa-times-circle"></i>';
+                                                            echo '</a>';
+                                                       echo'</button>';
+                                                    echo'</div>';
+                                            }
+                                            echo '</h4>';
+                                        echo '</div>';
+
+                                         echo '<div id="collapse'.$Collapse_Alphabet[$i].'" class="panel-collapse collapse in">';
+                                            echo '<div class="panel-body"> ';
+                                                echo '<div class="table-responsive">';
+                                                    echo '<table class="table table-striped table-hover">';
+                                                        echo '<thead>';
+                                                        echo '<tr>';
+                                                            echo '<th>#</th>';
+                                                            echo '<th>姓名</th>';
+                                                            echo '<th>学号</th>';
+                                                            echo '<th>操作</th>';
+                                                        echo '</tr>';
+                                                        echo '</thead>';
+                                                        echo '<tbody>';
+                                        $query = 'select student_id from student_class_group WHERE class_id = 1 AND group_id = '.$group_id;
+                                        $result = mysqli_query($conn, $query);
+                                        $student_id_array = array();
+                                        while ($row = mysqli_fetch_assoc($result)) array_push($student_id_array, $row['student_id']);
+                                        for($j=0; $j < count($student_id_array) ; $j++ )
+                                        {
+                                            $no = $j + 1;
+                                            $student_id = $student_id_array[$j];
+                                            $query = "select stu_number from people WHERE people.people_id = $student_id ";
+                                            $result = mysqli_query($conn, $query);
+                                            $stu_number = mysqli_fetch_assoc($result)['stu_number'];
+                                            $query = 'select name from people WHERE people.people_id = '.$student_id ;
+                                            $result = mysqli_query($conn, $query);
+                                            $name = mysqli_fetch_assoc($result)['name'];
+                                                        echo '<tr>';
+                                                            echo '<td>'.$no.'</td>';
+                                                            echo '<td>'.$name.'</td>';
+                                                            echo '<td>'.$stu_number.'</td>';
+                                                            echo '<td><a href="group_.php">删除 </td></a>';
+                                                        echo '</tr>';
+                                        }
+                                                        echo '</tbody>';
+                                                    echo '</table>';
+                                                echo '</div>';
+                                            echo '</div>';
+                                        echo '</div>';
+                                echo '</div>';
+                                }
+                                ?>
+                                <!-- /.panel-info -->
+                            </div>
                         </div>
-                        <center><button class="btn btn-success" type="submit">确认添加</button></center>
-                        </form>
+                        <!-- .panel-body -->
                     </div>
+                    <!-- /.panel -->
                 </div>
+                <!-- /.col-lg-12 -->
             </div>
-            <div class="col-lg-8" style="line-height: 300%">
-                <div class="panel panel-primary">
-                    <div class="panel-heading">
-                        <strong>添加小组</strong>
-                    </div>
-                    <div class="panel-body">
-                        <form method="post" action="">
-                            <div class="form-group input-group">
-                                <span class="input-group-addon">小组名</span>
-                                <input class="form-control" type="text" placeholder="name" name="group_name">
-                            </div>
-                            <div class="form-group input-group">
-                                <span class="input-group-addon">小组序号</span>
-                                <input class="form-control" type="text" placeholder="id" name="group_id">
-                            </div>
-                            <center><button class="btn btn-sm" type="submit">确认添加</button></center>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <!-- /.col-lg-4 -->
-        </div>
         <!-- /.row -->
     </div>
     <!-- /#page-wrapper -->
@@ -421,6 +466,3 @@ require_once('../common/mysql_connect.php');
 </body>
 
 </html>
-
-
-
