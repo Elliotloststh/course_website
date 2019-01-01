5ae819f9c3eb9ad1c6a39785bb1d8d80b0767417
ZigZag <elliotloststh@gmail.com>
2019-01-01 14:26:32 +0800

    Update homework_correct.php

--- teacher/homework_correct.php
+++ teacher/homework_correct.php
@@ -1,8 +1,44 @@
 <?php
 session_start();
 require_once('../common/mysql_connect.php');
-$homeworkId = $_GET['homework_id'];
 $teacherId = $_SESSION['people_id'];
+if(isset($_GET['homework_id'])) {
+    $homework_id = $_GET['homework_id'];
+    $query = "select * from tea_hw where homework_id=".$homework_id;
+    $result = mysqli_query($conn, $query);
+    if(mysqli_num_rows($result)==0) {
+        header('HTTP/1.1 404 Not Found');exit('404 Not Found');
+    } else {
+        $row = mysqli_fetch_assoc($result);
+        $title = $row['title'];
+        $resume = $row['homework_resume'];
+        $deadline = $row['deadline'];
+        $deadline_t = strtotime($deadline);
+        $address = $row['address'];
+    }
+} else {
+    header('HTTP/1.1 404 Not Found');exit('404 Not Found');
+}
+$query = "select * from stu_hw where homework_id=".$homework_id." and student_id=".$_SESSION['people_id'];
+$result = mysqli_query($conn, $query);
+if(mysqli_num_rows($result)==0) {
+    $flag = false;
+    $stu_file_name = "无";
+} else {
+    $row = mysqli_fetch_assoc($result);
+    $flag = true;
+    $stu_address = $row['local_address'];
+    $stu_file_name = $row['hw_name'];
+    $stu_names = explode('/',$stu_address);
+
+    $stu_method = "../common/download.php?filename=";
+    for($i=0;$i<count($stu_names);++$i) {
+        $stu_method = $stu_method.$stu_names[$i];
+        if($i<count($stu_names)-1) {
+            $stu_method=$stu_method.'%2F';
+        }
+    }
+}
 require ("sidebar_tea.php");
 ?>
 
@@ -50,6 +86,65 @@ require ("sidebar_tea.php");
 									mysqli_close($conn);
 								?>							
 							</table>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-9">
+                <div class="panel panel-primary">
+                    <div class="panel-heading">
+                        <p>
+                            作业信息
+                        </p>
+                    </div>
+                    <div class="panel-body">
+                        <table class="table table-striped table-hover">
+                            <tbody>
+                            <?php
+                            function download($local) {
+                                $file=fopen($local,"r");
+                                header("Content-Type: application/octet-stream");
+                                header("Accept-Ranges: bytes");
+                                header("Accept-Length: ".filesize('文件地址'));
+                                header("Content-Disposition: attachment; filename=文件名称");
+                                echo fread($file,filesize('文件地址'));
+                                fclose($file);
+                            }
+                            echo '<tr>';
+                            $print = "<td><b>描述</b></td>"; echo $print;
+                            echo '</tr>';
+                            echo '<tr>';
+                            $print = "<td>$resume</td>"; echo $print;
+                            echo '</tr>';
+                            echo '<tr>';
+                            $print = "<td><b>截止时间</b></td>"; echo $print;
+                            echo '</tr>';
+                            echo '<tr>';
+                            $print = "<td>$deadline</td>"; echo $print;
+                            echo '</tr>';
+                            echo '<tr>';
+                            $print = "<td><b>作业资料</b></td>"; echo $print;
+                            echo '</tr>';
+                            if($address==NULL) {
+                                echo '<tr>';
+                                $print = "<td>无</td>"; echo $print;
+                                echo '</tr>';
+                            } else {
+                                $names = explode('/',$address);
+                                $name = $names[sizeof($names)-1];
+                                $method = "../common/download.php?filename=";
+                                for($i=0;$i<count($names);++$i) {
+                                    $method = $method.$names[$i];
+                                    if($i<count($names)-1) {
+                                        $method=$method.'%2F';
+                                    }
+                                }
+                                echo '<tr>';
+                                $print = "<td><a href=\"$method\">$name</a></td>"; echo $print;
+                                echo '</tr>';
+                            }
+                            ?>
+                            </tbody>
+                        </table>
                     </div>
                 </div>
             </div>
